cb9dde97c9bdf38d4ccb205c018a46c00a057439
maxim1770 <maxim1770@gmail.com>
2022-07-18 11:32:56 +0300

    добавил merge файлов

--- parsing_manuscript_info/work_branch/work_with_json_data_books_info.py
+++ parsing_manuscript_info/work_branch/work_with_json_data_books_info.py
@@ -19,35 +19,30 @@ def work_with_data_book_info(data_book_info: dict):
     return data_book_info
 
 
-def write_edit_data_book_json_book(edited_data_book: dict, title_book_eng_for_link: str, cur_time: str):
-    with open(f'data/data_books/books/{title_book_eng_for_link}/{cur_time}/edit_data_book.json', "w",
+def write_data_book_json_book(version: str, data_book: dict, title_book_eng_for_link: str, cur_time: str):
+    with open(f'data/data_books/books/{title_book_eng_for_link}/{cur_time}/{version}_data_book.json', "w",
               encoding="utf-8") as json_file:
-        json.dump(edited_data_book, json_file, indent=4, ensure_ascii=False)
+        json.dump(data_book, json_file, indent=4, ensure_ascii=False)
 
 
-def read_edit_data_book_json(title_book_eng_for_link: str, cur_time: str):
-    with open(
-            f'data/data_books/books/{title_book_eng_for_link}/{cur_time}/edit_data_book.json',
-            encoding='utf-8') as json_file:
-        edited_data_book = json.load(json_file)
-
-    return edited_data_book
-
-
-def write_edit_data_book_json_books(edited_data_book: dict, cur_time: str, count_iteration: int):
-    with open(f'data/data_books/all_books/{cur_time}/edit_data_books.json', "a",
+def write_data_book_json_books(version: str, data_book: dict, cur_time: str, count_iteration: int):
+    with open(f'data/data_books/all_books/{cur_time}/{version}_data_books.json', "a",
               encoding="utf-8") as json_file:
         if count_iteration != 0:
             json_file.write(',\n')
-        json.dump(edited_data_book, json_file, indent=4, ensure_ascii=False)
+        json.dump(data_book, json_file, indent=4, ensure_ascii=False)
 
 
-def read_data_books_json(cur_time: str):
-    with open(f'data/data_books/all_books/{cur_time}/collect_data_books.json',
+def read_data_books_json(version: str, cur_time: str):
+    with open(f'data/data_books/all_books/{cur_time}/{version}_data_books.json',
               encoding="utf-8") as json_file:
-        data_books = json.load(json_file)
+        return json.load(json_file)
 
-    return data_books
+
+def read_data_book_json(version: str, title_book_eng_for_link: str, cur_time: str):
+    with open(f'data/data_books/books/{title_book_eng_for_link}/{cur_time}/{version}_data_book.json',
+              encoding="utf-8") as json_file:
+        return json.load(json_file)
 
 
 def edit_data_book_title(data_book_info: dict):
@@ -102,7 +97,6 @@ def edit_data_book_info(data_book_info):
     edited_data_book_info.update(edit_data_book_sheets_number(data_book_info))
     edited_data_book_info.update(edit_data_book_owner(data_book_info))
 
-
     return edited_data_book_info
 
 
@@ -111,7 +105,7 @@ def manager_edit_data_book(data_book, cur_time):
     edited_data_book['title_book_eng_for_link'] = data_book['title_book_eng_for_link']
     edited_data_book['info'] = edit_data_book_info(data_book['info'])
 
-    write_edit_data_book_json_book(edited_data_book, data_book['title_book_eng_for_link'], cur_time)
+    write_data_book_json_book('edit', edited_data_book, data_book['title_book_eng_for_link'], cur_time)
 
 
 def manager_edit_data_books(data_books, cur_time):
@@ -129,9 +123,9 @@ def manager_edit_data_books(data_books, cur_time):
 
         manager_edit_data_book(data_book, cur_time)
 
-        edited_data_book = read_edit_data_book_json(title_book_eng_for_link, cur_time)
+        edited_data_book = read_data_book_json('edit', title_book_eng_for_link, cur_time)
 
-        write_edit_data_book_json_books(edited_data_book, cur_time, count_iteration)
+        write_data_book_json_books('edit', edited_data_book, cur_time, count_iteration)
 
         # -----
         count_iteration += 1
@@ -145,10 +139,56 @@ def manager_edit_data_books(data_books, cur_time):
         json_file.write('\n]')
 
 
+def manager_merge_json_files(title_book_eng_for_link, cur_time: str):
+    data_book = read_data_book_json("collect", title_book_eng_for_link, cur_time)
+
+    edited_data_book = read_data_book_json("edit", title_book_eng_for_link, cur_time)
+
+    finished_data_book = {}
+
+    finished_data_book.update(data_book)
+    finished_data_book.update(edited_data_book)
+
+    write_data_book_json_book('finished', finished_data_book, title_book_eng_for_link, cur_time)
+
+
+def manager_merges_json_files(data_books, cur_time: str):
+    with open(f'data/data_books/all_books/{cur_time}/finished_data_books.json', "w",
+              encoding="utf-8") as json_file:
+        json_file.write('[\n')
+    # -----
+    count_iteration = 0
+    # -----
+    for data_book in data_books[:2]:
+        title_book_eng_for_link = data_book['title_book_eng_for_link']
+        # ---
+        print(f"INFO, Итерация: {count_iteration}, Название: {title_book_eng_for_link}")
+        # ---
+
+        manager_merge_json_files(title_book_eng_for_link, cur_time)
+
+        finished_data_book = read_data_book_json('finished', title_book_eng_for_link, cur_time)
+
+        write_data_book_json_books('finished', finished_data_book, cur_time, count_iteration)
+
+        # -----
+        count_iteration += 1
+        # -----
+        # ---
+        print(*["-" * 20] * 3, sep='\n')
+        # ---
+
+    with open(f'data/data_books/all_books/{cur_time}/finished_data_books.json', "a",
+              encoding="utf-8") as json_file:
+        json_file.write('\n]')
+
+
 def main():
-    cur_time = '18_07_2022_00_40'
-    data_books = read_data_books_json(cur_time)
+    cur_time = '18_07_2022_11_26'
+    data_books = read_data_books_json("collect", cur_time)
     manager_edit_data_books(data_books, cur_time)
+
+    manager_merges_json_files(data_books, cur_time)
 
 
 if __name__ == "__main__":
